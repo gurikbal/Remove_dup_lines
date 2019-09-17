c0e5ae0ac040d24273f9b8a6d184e51c7ecb673c
gurikbal <36083297+gurikbal@users.noreply.github.com>
2019-09-17 14:53:42 +0530

    fixed creating new line

--- Remove dup lines/Remove dup lines/Main.cs
+++ Remove dup lines/Remove dup lines/Main.cs
@@ -35,7 +35,7 @@ namespace Remove_dup_lines
         #region " Menu functions "
         internal static void about()
         {
-            var ss = "       Remove Duplicate lines Except Empty lines  \n                          build by G. Singh  \n                      02-09-2019 build 1.0.8.0  ";
+            var ss = "                   To Remove all visible Duplicate lines Use \n              Edit > Blank Operations > Trim Trailing Space \n\n       ****** Remove Duplicate lines Except Empty lines ******  \n                                       build by G. Singh  \n                                  17-09-2019 build 1.0.9.0  ";
             MessageBox.Show(ss);
         }
         internal static void selection()
@@ -62,7 +62,7 @@ namespace Remove_dup_lines
                         x += 1;
                         if (string.IsNullOrEmpty(line.Trim()))
                         {
-                            line = Regex.Replace(line, "", "3f5456cfsd661lld3334349j55kkk000000000000000000000000000000000000jgggggjyyyyyy" + x);
+                            line = Regex.Replace(line, "$", "3f5456cfsd661lld33Guid9CA0F324-3E3A-4C43-8AGu989CAACD-3BD6-499D-8664-41CE47622FE25E1E7C8FF3Guid9CA0F324-3E3A-4C43-8A22-308B5E1E7C8FA-4C43-8A22-308B5E1E7CGuid9CA0F324-3E3A-4C43-8A22-308B5E1E7C8F" + x);
                         }
                         writer.WriteLine(line);
                     }
@@ -78,12 +78,18 @@ namespace Remove_dup_lines
                     string linek;
                     while ((linek = readerr.ReadLine()) != null)
                     {
-                        string uu = Regex.Replace(linek, "^3f5456cfsd661lld3334349j55kkk000000000000000000000000000000000000jgggggjyyyyyy.*$", "");
+                        string uu = Regex.Replace(linek, "3f5456cfsd661lld33Guid9CA0F324-3E3A-4C43-8AGu989CAACD-3BD6-499D-8664-41CE47622FE25E1E7C8FF3Guid9CA0F324-3E3A-4C43-8A22-308B5E1E7C8FA-4C43-8A22-308B5E1E7CGuid9CA0F324-3E3A-4C43-8A22-308B5E1E7C8F.*$", "");
                         writerr.WriteLine(uu);
                     }
                     outputStrin = writerr.ToString();
+                    outputStrin = outputStrin.Substring(0, outputStrin.LastIndexOf(Environment.NewLine));
                 }
+                if (inputText.ToString() == outputStrin)
+                {
 
+                    Win32.SendMessage(PluginBase.GetCurrentScintilla(), SciMsg.SCI_CLEARSELECTIONS, 0, 0);
+                    return;
+                }
                 Win32.SendMessage(PluginBase.GetCurrentScintilla(), SciMsg.SCI_REPLACESEL, 0, outputStrin);
             }
             catch (Exception exp)
